859e36402315abd2b5c88071badf9636afbd91f2
Eduardo Medeiros Martins <eduardomartttins@gmail.com>
2024-06-11 21:41:16 -0300

    att

--- sistema-estoque/src/Componentes/Historico/Historico.jsx
+++ sistema-estoque/src/Componentes/Historico/Historico.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 
-const Historico = () =>
-{return(
-    <div>
-        <h1>Olá mundo!</h1>
-    </div>
-);}
+const Historico = () => {
+    return(
+        <div>
+            <h1>Olá mundo!</h1>
+        </div>
+    );
+}
 
 export default Historico;
