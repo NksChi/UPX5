4bb9a57f134d7e2a975e9f27eaf8da9e90b5e97f
Eduardo Medeiros Martins <eduardomartttins@gmail.com>
2024-06-22 13:44:53 -0300

    att

--- sistema-estoque/src/Componentes/Cadastro/Cadastro.jsx
+++ sistema-estoque/src/Componentes/Cadastro/Cadastro.jsx
@@ -38,81 +38,84 @@ const Cadastro = () => {
   };
 
   return (
-    <div className="register">
-      <Navbar handleLogout={handleLogout} />
-      <div className="page-container">
-        <h1>Cadastro de Equipamentos</h1>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="nomeProduto">Nome do produto:</label>
-          <input
-            type="text"
-            id="nomeProduto"
-            value={nomeProduto}
-            onChange={(event) => setNomeProduto(event.target.value)}
-          />
+    <div>
+       <div className="register">
+        <Navbar handleLogout={handleLogout} />
+        <div className="page-container">
+          <h1>Cadastro de Equipamentos</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="nomeProduto">Nome do produto:</label>
+            <input
+              type="text"
+              id="nomeProduto"
+              value={nomeProduto}
+              onChange={(event) => setNomeProduto(event.target.value)}
+            />
 
-          <label htmlFor="marca">Marca:</label>
-          <input
-            type="text"
-            id="marca"
-            value={marca}
-            onChange={(event) => setMarca(event.target.value)}
-          />
+            <label htmlFor="marca">Marca:</label>
+            <input
+              type="text"
+              id="marca"
+              value={marca}
+              onChange={(event) => setMarca(event.target.value)}
+            />
 
-          <label htmlFor="valor">Valor:</label>
-          <input
-            type="number"
-            id="valor"
-            value={valor}
-            onChange={(event) => setValor(event.target.value)}
-          />
+            <label htmlFor="valor">Valor:</label>
+            <input
+              type="number"
+              id="valor"
+              value={valor}
+              onChange={(event) => setValor(event.target.value)}
+            />
 
-          <label htmlFor="numeroSerie">N° de série:</label>
-          <input
-            type="text"
-            id="numeroSerie"
-            value={numeroSerie}
-            onChange={(event) => setNumeroSerie(event.target.value)}
-          />
+            <label htmlFor="numeroSerie">N° de série:</label>
+            <input
+              type="text"
+              id="numeroSerie"
+              value={numeroSerie}
+              onChange={(event) => setNumeroSerie(event.target.value)}
+            />
 
-          <label htmlFor="valorS">Valor Sugerido:</label>
-          <input
-            type="number"
-            id="valorS"
-            value={valorS}
-            onChange={(event) => setValorS(event.target.value)}
-          />
+            <label htmlFor="valorS">Valor Sugerido:</label>
+            <input
+              type="number"
+              id="valorS"
+              value={valorS}
+              onChange={(event) => setValorS(event.target.value)}
+            />
 
-          <label htmlFor="tipo">Tipo:</label>
-          <select
-            id="tipo"
-            value={tipo}
-            onChange={(event) => setTipo(event.target.value)}
-          >
-            <option value="Servidor">Servidor</option>
-            <option value="Notebook">Notebook</option>
-            <option value="Impressora">Impressora</option>
-            <option value="Outro">Outro</option>
-          </select>
+            <label htmlFor="tipo">Tipo:</label>
+            <select
+              id="tipo"
+              value={tipo}
+              onChange={(event) => setTipo(event.target.value)}
+            >
+              <option value="Servidor">Servidor</option>
+              <option value="Notebook">Notebook</option>
+              <option value="Impressora">Impressora</option>
+              <option value="Outro">Outro</option>
+            </select>
 
-          <label htmlFor="dataFabricacao">Data de fabricação:</label>
-          <input
-            type="date"
-            id="dataFabricacao"
-            value={dataFabricacao}
-            onChange={(event) => setDataFabricacao(event.target.value)}
-          />
+            <label htmlFor="dataFabricacao">Data de fabricação:</label>
+            <input
+              type="date"
+              id="dataFabricacao"
+              value={dataFabricacao}
+              onChange={(event) => setDataFabricacao(event.target.value)}
+            />
 
-          <button type="submit">Cadastrar</button>
-        </form>
-      </div>
-      <div className="image-container">
-        <img src={Server} alt="Server" className="top-image" />
-        <div className="footer-regis">
-          &copy; {new Date().getFullYear()} TechLifeCycle. Todos os direitos reservados. <span className='upx'> / UPX5 - GRUPO 8 </span>
+            <button type="submit">Cadastrar</button>
+          </form>
+        </div>
+        <div className="image-container">
+          <img src={Server} alt="Server" className="top-image" />
         </div>
       </div>
+      <div className="footer-regis">
+            &copy; {new Date().getFullYear()} TechLifeCycle. Todos os direitos reservados. <span className='upx'> / UPX5 - GRUPO 8 </span>
+      </div>
     </div>
+   
   );
 };
 
